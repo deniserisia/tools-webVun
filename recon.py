8e9b20c5d1dbc8f493d9403605737b46daa16a9a
DeniseRisia <risiaresponde@gmail.com>
2023-10-20 10:02:53 -0300

    atualização list

--- recon.py
+++ recon.py
@@ -1,13 +1,15 @@
 import requests
-url = "http://localhost:4200"
+url = "http://localhost:4200/"
 
+# pip install requests
+# Discovery -> WebContent -> common.txt
 #lista_de_arquivos = open('lista-de-arquivos-angular.txt')
-lista_de_arquivos = open('common.txt')
+lista_de_arquivos = open('quickhits.txt')
 
 for linha in lista_de_arquivos.readlines():
     url_teste = url + "/" + linha.replace("\n", "")
     requisicao = requests.get(url_teste)
-    if(requisicao.status_code == 400):
+    if(requisicao.status_code == 404):
         continue
     if(requisicao.status_code == 200):
         print("[+] Diretório ou arquivo encontraro: ", linha.replace("\n", ""))
